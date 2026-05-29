21c8bf0df09ef33ff3a98dc3edb9d5dea8bc984e
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-30 02:26:26 +0530

    implement fetchCharacters function to fetch Card images

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,38 @@ function App() {
   const [score, setScore] = useState(0)
   const [highScore, setHighScore] = useState(0)
   const [win, setWin] = useState(false)
-  const [characters, setCharacters] = useState([])
+  const [cards, setCards] = useState([])
   
   function shuffleCards() {
     const shuffledCards = [...cards].sort( ()=> Math.random() - 0.5)
   }
+
+  const apiUrl = "https://genshin.jmp.blue/"
+
+  useEffect( () => {
+    async function fetchCharacters() {
+      //fetching all character names from the API
+      const names = await fetch(apiUrl + "characters").then((res) => res.json())
+
+      const validCards = [] //temp-variable to store valid cards before setting state
+
+      //checking if the character has an image, if it does, we add it to the validCards array
+      for(let i=0; i<names.length; i++){
+                          
+        const characterName = names[i]
+        //storing the image url in a var
+        const imgURL=  `${apiUrl}characters/${characterName}/icon`
+        //checking if the image url is valid 
+        const imageCheck = await fetch(imgURL)
+        // if success, add to the temp var - ValidCards []
+        if(imageCheck.ok){
+          validCards.push({id: i, name:characterName, img:imgURL})
+        }
+      }
+      setCards(validCards.slice(0, 12)) //only taking the first 12 valid cards
+  }
+    fetchCharacters()
+  }, [])
 
   return (
     <>
