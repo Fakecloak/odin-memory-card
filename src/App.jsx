aa7e31bddbdf46a9481dfa63e4918d86f7bdc319
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-29 23:56:53 +0530

    initiliaze state variables and shuffleCards func

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/App.css'
 
 function App() {
   const [score, setScore] = useState(0)
   const [highScore, setHighScore] = useState(0)
   const [win, setWin] = useState(false)
+  const [characters, setCharacters] = useState([])
   
+  function shuffleCards() {
+    const shuffledCards = [...cards].sort( ()=> Math.random() - 0.5)
+  }
 
   return (
     <>
