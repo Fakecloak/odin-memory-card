546acb19355c42303f330696fbab1d9404b2c5ce
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-30 18:02:28 +0530

    refactor shuffleCards logic

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,30 @@ function App() {
   const [clickedCards, setClickedCards] = useState([])
   
   function shuffleCards(cardArray) {
-    const shuffledCards = [...cardArray].sort( ()=> Math.random() - 0.5).slice(0,12)
-    // const shuffledCards = cardArray.slice(0,12);
+    const shuffledCards = [...cardArray].sort( ()=> Math.random() - 0.5)
     setCards(shuffledCards)
   }
 
   function handleCardClick(id) {
     if(clickedCards.includes(id)){
+
       console.log("Card already clicked! Game over.")
       setScore(0) //reset score to 0
       setClickedCards([])  //reset clicked cards
 
     } else {
-    setClickedCards([...clickedCards, id])
-    setHighScore( highScore + 1)
-    setScore(score + 1)
 
-    console.log(clickedCards)
+      const newScore = score + 1;
+
+      setClickedCards([...clickedCards, id])  
+      setScore(newScore)
+      console.log(clickedCards)
+
+      if(newScore > highScore) {
+      setHighScore(newScore)
+      } 
     }
+    shuffleCards(cards)
   }
 
   const apiUrl = "https://genshin.jmp.blue/"
@@ -42,7 +48,7 @@ function App() {
         img: `${apiUrl}characters/${characterName}/icon`
 
       }))
-      shuffleCards(validCards) //shuffle the cards before setting state
+      shuffleCards(validCards.slice(0, 12)) //shuffle the cards before setting state
     }
     fetchCharacters()
   }, [])
